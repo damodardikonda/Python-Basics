196375cb76f8ea639fcb65e4c0af840d61b0da8d
unknown <damodar2dikonda@gmail.com>
2020-06-24 21:28:29 +0530

    Python Basic

---
+++ else_break.py
@@ -0,0 +1,8 @@
+list=[1,2,3,4,5]
+
+for i in list:
+    print(i)
+    if i==5:
+      break
+else:
+      print("this is not wnt to print")
---
+++ while_break.py
@@ -0,0 +1,8 @@
+i=1
+while i<=45:
+    print(i)
+    i+=1
+    if i==9:
+        break
+else:
+    print("this code is not running as expected");
